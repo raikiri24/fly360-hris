05d4fa30fc0c75151f8e7668a74ae0e99533445f
Paul Leandro Lanot <paul.leanot.aesiph@gmail.com>
2023-07-04 09:08:51 +0800

    initial state

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,9 @@
 import { React } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Login from './components/Login/Login';
-import Navigation from './components/Navigation/Navigation';
-import Whatsnew from './components/WhatsNew/Whatsnew';
-import SideBar from './components/SideBar/SideBar';
 
 function App() {
   return (
-    <div className="App bg-[#EEEEEE]">
-      {/* <Login /> */}
-
-      <Navigation />
-      <div className="relative">
-        <SideBar />
-        <Whatsnew />
-      </div>
+    <div className="App ">
+     
     </div>
   );
 }
