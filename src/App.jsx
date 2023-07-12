d472d07255685cfa3ba2f9327bcaa4f9198c60c0
Paul Leandro Lanot <31089473+raikiri24@users.noreply.github.com>
2023-07-12 20:30:21 +0800

    added footer and dropdown (still in progress)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { React } from 'react';
 import Navigation from './components/Navigation/Navigation';
 import HRISNavigation from './components/HRIS-Navigation/HRISNavigation';
-
+import Footer from './components/Footer/Footer';
+import 'rsuite/dist/rsuite-no-reset.min.css';
 function App() {
   return (
     <div className="App ">
       <Navigation />
       <HRISNavigation />
+      <Footer />
     </div>
   );
 }
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,26 +1,36 @@
-import React from 'react'
-import {IoMdNotifications} from 'react-icons/io'
-import {RxAvatar} from 'react-icons/rx'
-import Logo from '../../assets/nav-icon-home.svg'
+import React from 'react';
+import { IoMdNotifications } from 'react-icons/io';
+import { RxAvatar } from 'react-icons/rx';
+import Logo from '../../assets/nav-icon-home.svg';
 
 const Navigation = () => {
   return (
-    <nav  className='font-custom bg-[#66A7A3] px-20 py-4'>
-        <div className='flex justify-between items-center'>
-            <div className='text-white text-lg'>
-               <img src={Logo} alt="" className='h-12'/>
-            </div>
-
-            <div className='flex gap-10 items-center'>
-                <span className='text-white text-2xl'><IoMdNotifications/></span>
-                <div className='flex gap-3 bg-[#00A59B] p-4 rounded-lg shadow-sm'>
-                    <span className='text-white text-lg'><RxAvatar/></span>
-                    <span className='text-white text-sm'>Paul Leandro Lanot</span>
-                </div>
-            </div>
+    <nav className="font-custom bg-[#66A7A3] px-10 py-4">
+      <div className="flex justify-between items-center">
+        <div className="text-white text-lg">
+          <img src={Logo} alt="" className="h-12" />
         </div>
-    </nav>
-  )
-}
 
-export default Navigation
+        <div className="flex gap-10 items-center">
+          <span className="text-white text-2xl">
+            <IoMdNotifications />
+          </span>
+          <div className="flex gap-3 bg-[#00A59B] p-4 rounded-lg shadow-sm cursor-pointer relative">
+            <span className="text-white text-lg">
+              <RxAvatar />
+            </span>
+            <span className="text-white text-sm">Paul Leandro Lanot</span>
+            <div className="absolute bg-white py-2  w-60 -left-14 top-14 shadow-xl z-50">
+              <ul>
+                <li className="px-6 py-2 hover:bg-gray-700 hover:text-white">Paul Leandro Lanot</li>
+                <li className="px-6 py-2 hover:bg-gray-700 hover:text-white">Logout</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navigation;
