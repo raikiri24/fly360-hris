27a7b40bc516dd28e117d81b9a198f358b424bfd
Paul Leandro Lanot <31089473+raikiri24@users.noreply.github.com>
2023-07-15 19:11:57 +0800

    added react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { React } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Navigation from './components/Navigation/Navigation';
 import HRISNavigation from './components/HRIS-Navigation/HRISNavigation';
 import Footer from './components/Footer/Footer';
 import 'rsuite/dist/rsuite-no-reset.min.css';
+
 function App() {
   return (
     <div className="App ">
       <Navigation />
-      <HRISNavigation />
+      <Routes>
+        <Route path="/" element={<HRISNavigation />} />
+      </Routes>
       <Footer />
     </div>
   );
