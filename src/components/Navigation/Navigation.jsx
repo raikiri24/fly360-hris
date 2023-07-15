6154ed557ce8b029b9d9aa7315bd206d4c781b9d
Paul Leandro Lanot <31089473+raikiri24@users.noreply.github.com>
2023-07-15 10:16:21 +0800

    added dropdown menu based on figma

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IoMdNotifications } from 'react-icons/io';
 import { RxAvatar } from 'react-icons/rx';
+import { HiSwitchHorizontal } from 'react-icons/hi';
+import { BiLogOut } from 'react-icons/bi';
 import Logo from '../../assets/nav-icon-home.svg';
 import { motion } from 'framer-motion';
 
 const Navigation = () => {
+  const [dropdownIsOpen, setDropdownIsOpen] = useState(false);
+  const [accountsIsOpen, setAccountsIsOpen] = useState(false);
   return (
-    <nav className="font-custom bg-[#66A7A3] px-10 py-4">
+    <nav className="font-custom bg-[#66A7A3] px-10 py-4 ">
       <div className="flex justify-between items-center">
         <div className="text-white text-lg">
           <img src={Logo} alt="" className="h-12" />
@@ -27,19 +31,52 @@ const Navigation = () => {
             <motion.button
               whileHover={{ scale: 1.0 }}
               whileTap={{ scale: 0.9 }}
-              className="flex gap-3 bg-[#00A59B] p-4 rounded-lg shadow-sm cursor-pointer">
+              className="flex gap-3 bg-[#00A59B] p-4 rounded-lg shadow-sm cursor-pointer"
+              onClick={() => setDropdownIsOpen(!dropdownIsOpen)}>
               <span className="text-white text-lg">
                 <RxAvatar />
               </span>
               <span className="text-white text-sm">Paul Leandro Lanot</span>
             </motion.button>
 
-            {/* <div className="absolute bg-white py-2  w-60 -left-14 top-14 shadow-xl z-50">
-              <ul>
-                <li className="px-6 py-2 hover:bg-gray-700 hover:text-white">Paul Leandro Lanot</li>
-                <li className="px-6 py-2 hover:bg-gray-700 hover:text-white">Logout</li>
-              </ul>
-            </div> */}
+            {dropdownIsOpen && (
+              <div className="absolute bg-white py-2  w-48 left-0 top-12 shadow-xl z-50 rounded-br-lg rounded-bl-lg">
+                <ul>
+                  <li
+                    className="px-2 py-2 grid grid-cols-5 gap-2 justify-center items-center cursor-pointer"
+                    onClick={() => setAccountsIsOpen(!accountsIsOpen)}>
+                    <motion.button
+                      whileHover={{ scale: 1.2 }}
+                      whileTap={{ scale: 0.9 }}
+                      className="bg-[#088585] hover:shadow-xl rounded-full w-6 h-6 col-span-1 flex justify-center items-center">
+                      <HiSwitchHorizontal className="text-white" />
+                    </motion.button>
+                    <span className="col-span-4">Switch Account</span>
+                  </li>
+                  {accountsIsOpen && (
+                    <div>
+                      <li className="px-2 py-2 hover:bg-gray-700 hover:text-white flex ml-5 items-center gap-2 text-xs cursor-pointer">
+                        <span>
+                          <RxAvatar />
+                        </span>
+                        Paul Leandro Lanot
+                      </li>
+                      <li className="px-2 py-2 hover:bg-gray-700 hover:text-white flex ml-5 items-center gap-2 text-xs cursor-pointer">
+                        <span>
+                          <RxAvatar />
+                        </span>
+                        Paul Leandro Lanot
+                      </li>
+                    </div>
+                  )}
+
+                  <li className="px-2 py-2 hover:bg-gray-700 hover:text-white flex items-center gap-2 text-amber-700 cursor-pointer">
+                    <BiLogOut />
+                    Logout
+                  </li>
+                </ul>
+              </div>
+            )}
           </div>
         </div>
       </div>
