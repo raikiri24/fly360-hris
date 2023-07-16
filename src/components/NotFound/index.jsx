f318e79e3973612c356c8d0ee43dc5b3b1c6fa6a
Paul Leandro Lanot <31089473+raikiri24@users.noreply.github.com>
2023-07-16 21:59:30 +0800

    fixed 404 page added footer

--- src/components/NotFound/index.jsx
+++ src/components/NotFound/index.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import Navigation from '../Navigation/Navigation';
+import Footer from '../Footer/Footer';
 
 const NotFound = () => {
   return (
     <div className="font-custom">
       <Navigation />
-      <div className="flex flex-col self-center items-center gap-5">
-        <div className="text-7xl">OOPS</div>
+      <div className="flex flex-col items-center justify-center gap-5 h-96 ">
+        <div className="text-7xl self-center">OOPS</div>
         <div className="text-4xl">404 - PAGE NOT FOUND</div>
         <div className="bg-fly-404 h-40 w-40 bg-contain bg-no-repeat"></div>
       </div>
+      <Footer />
     </div>
   );
 };
