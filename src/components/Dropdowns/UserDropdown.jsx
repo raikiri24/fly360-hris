5bd078e56e677e994cf63eb524c85e6397bc042b
Paul Leandro Lanot <31089473+raikiri24@users.noreply.github.com>
2023-07-19 19:27:52 +0800

    new dropdown design

---
+++ src/components/Dropdowns/UserDropdown.jsx
@@ -0,0 +1,104 @@
+import React, { useState } from 'react';
+import { HiSwitchHorizontal } from 'react-icons/hi';
+import { BiLogOut } from 'react-icons/bi';
+import { VscSettings } from 'react-icons/vsc';
+import { HiKey } from 'react-icons/hi';
+import { HiOutlineMoon } from 'react-icons/hi';
+import { motion } from 'framer-motion';
+import { useLogin } from '../../context/LoginContext';
+import { RxAvatar } from 'react-icons/rx';
+import { Tooltip, Whisper } from 'rsuite';
+
+const TooltipComponent = () => (
+  <Whisper
+    trigger="hover"
+    placement="left"
+    preventOverflow
+    speaker={<Tooltip>Switch to Admin</Tooltip>}>
+    <motion.button
+      whileHover={{ scale: 1.2 }}
+      whileTap={{ scale: 0.9 }}
+      className="bg-[#088585] hover:shadow-xl rounded-full w-6 h-6 col-span-1 flex justify-center items-center">
+      <HiSwitchHorizontal className="text-white" />
+    </motion.button>
+  </Whisper>
+);
+
+const UserDropdown = () => {
+  const [accountsIsOpen, setAccountsIsOpen] = useState(false);
+  const [activeAccount, setActiveAccount] = useState(true);
+
+  const { handleLogout } = useLogin();
+  return (
+    <div className="absolute bg-[#66A7A3] py-2  w-80 right-0 top-12 shadow-xl z-50 rounded-br-lg rounded-bl-lg">
+      <ul>
+        <li className="px-2 " onClick={() => setAccountsIsOpen(!accountsIsOpen)}>
+          <div className=" px-2 py-5 rounded-md bg-[#177E8C] grid grid-cols-5 gap-2 justify-center items-center cursor-pointer bg-opacity-30">
+            <span className="col-span-4 text-white flex items-center gap-2">
+              <RxAvatar className="text-2xl" />
+              Paul Leandro Lanot
+            </span>
+            <TooltipComponent />
+          </div>
+        </li>
+        {accountsIsOpen && (
+          <div>
+            <li
+              className={
+                activeAccount
+                  ? 'bg-slate-700 text-white px-5 py-2 hover:bg-[#177E8C] hover:bg-opacity-20 flex  items-center gap-2 text-xs cursor-pointer '
+                  : 'px-5 py-2 hover:bg-gray-400 hover:text-white flex ml-5 items-center gap-2 text-xs cursor-pointer '
+              }>
+              <span>
+                <RxAvatar />
+              </span>
+              Super Admin Mode
+            </li>
+            <li className="px-5 py-2 hover:bg-gray-400 hover:text-white flex  items-center gap-2 text-xs cursor-pointer">
+              <span>
+                <RxAvatar />
+              </span>
+              Admin Mode
+            </li>
+            <li className="px-5 py-2 hover:bg-gray-400 hover:text-white flex items-center gap-2 text-xs cursor-pointer">
+              <span>
+                <RxAvatar />
+              </span>
+              Employee Mode
+            </li>
+          </div>
+        )}
+
+        <li className="px-2 py-2 hover:bg-[#177E8C] hover:bg-opacity-20 flex items-center gap-2 text-white cursor-pointer">
+          <span className=" rounded-full bg-[#2B646C] bg-opacity-30 p-2">
+            <HiKey />
+          </span>
+          Change Password
+        </li>
+        <li className="px-2 py-2 hover:bg-[#177E8C] hover:bg-opacity-20 flex items-center gap-2 text-white cursor-pointer">
+          <span className=" rounded-full bg-[#2B646C] bg-opacity-30 p-2">
+            <HiOutlineMoon />
+          </span>
+          Display & Access
+        </li>
+
+        <li className="px-2 py-2 hover:bg-[#177E8C] hover:bg-opacity-20 flex items-center gap-2 text-white cursor-pointer">
+          <span className=" rounded-full bg-[#2B646C] bg-opacity-30 p-2">
+            <VscSettings />
+          </span>
+          Settings
+        </li>
+        <li
+          className="px-2 py-2 hover:bg-[#177E8C] hover:bg-opacity-20 flex items-center gap-2 text-white cursor-pointer"
+          onClick={() => handleLogout()}>
+          <span className=" rounded-full bg-[#2B646C] bg-opacity-30 p-2">
+            <BiLogOut />
+          </span>
+          <span className="text-amber-400">Logout</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default UserDropdown;
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,18 +1,15 @@
 import React, { useState } from 'react';
 import { IoMdNotifications } from 'react-icons/io';
 import { RxAvatar } from 'react-icons/rx';
-import { HiSwitchHorizontal } from 'react-icons/hi';
-import { BiLogOut } from 'react-icons/bi';
-import { VscSettings } from 'react-icons/vsc';
+
 import Logo from '../../assets/nav-icon-home.svg';
 import { motion } from 'framer-motion';
-import { useLogin } from '../../context/LoginContext';
+
+import UserDropdown from '../Dropdowns/UserDropdown';
 
 const Navigation = () => {
   const [dropdownIsOpen, setDropdownIsOpen] = useState(false);
-  const [accountsIsOpen, setAccountsIsOpen] = useState(false);
-  const [activeAccount, setActiveAccount] = useState(true);
-  const { handleLogout } = useLogin();
+
   return (
     <nav className="font-custom bg-[#66A7A3] px-10 py-4 ">
       <div className="flex justify-between items-center">
@@ -20,7 +17,7 @@ const Navigation = () => {
           <img src={Logo} alt="" className="h-12" />
         </div>
 
-        <div className="flex gap-10 items-center">
+        <div className="flex gap-10 items-center justify-center">
           <motion.button
             whileHover={{ scale: 1.0 }}
             whileTap={{ scale: 0.9 }}
@@ -31,73 +28,17 @@ const Navigation = () => {
             <div className=" rounded-full h-2 w-2 bg-red-800 absolute top-0 right-0"></div>
           </motion.button>
 
-          <div className="  relative">
+          <div className="relative flex justify-center">
             <motion.button
               whileHover={{ scale: 1.0 }}
               whileTap={{ scale: 0.9 }}
-              className="flex gap-3 bg-[#00A59B] p-4 rounded-lg shadow-sm cursor-pointer"
+              className="rounded-lg shadow-sm cursor-pointer"
               onClick={() => setDropdownIsOpen(!dropdownIsOpen)}>
-              <span className="text-white text-lg">
+              <span className="text-white text-2xl">
                 <RxAvatar />
               </span>
-              <span className="text-white text-sm">Paul Leandro Lanot</span>
             </motion.button>
-
-            {dropdownIsOpen && (
-              <div className="absolute bg-white py-2  w-48 left-0 top-12 shadow-xl z-50 rounded-br-lg rounded-bl-lg">
-                <ul>
-                  <li
-                    className="px-2 py-2 grid grid-cols-5 gap-2 justify-center items-center cursor-pointer"
-                    onClick={() => setAccountsIsOpen(!accountsIsOpen)}>
-                    <motion.button
-                      whileHover={{ scale: 1.2 }}
-                      whileTap={{ scale: 0.9 }}
-                      className="bg-[#088585] hover:shadow-xl rounded-full w-6 h-6 col-span-1 flex justify-center items-center">
-                      <HiSwitchHorizontal className="text-white" />
-                    </motion.button>
-                    <span className="col-span-4">Switch Account</span>
-                  </li>
-                  {accountsIsOpen && (
-                    <div>
-                      <li
-                        className={
-                          activeAccount
-                            ? 'bg-slate-700 text-white px-5 py-2 hover:bg-gray-700 hover:text-white flex  items-center gap-2 text-xs cursor-pointer '
-                            : 'px-5 py-2 hover:bg-gray-400 hover:text-white flex ml-5 items-center gap-2 text-xs cursor-pointer '
-                        }>
-                        <span>
-                          <RxAvatar />
-                        </span>
-                        Super Admin Mode
-                      </li>
-                      <li className="px-5 py-2 hover:bg-gray-400 hover:text-white flex  items-center gap-2 text-xs cursor-pointer">
-                        <span>
-                          <RxAvatar />
-                        </span>
-                        Admin Mode
-                      </li>
-                      <li className="px-5 py-2 hover:bg-gray-400 hover:text-white flex items-center gap-2 text-xs cursor-pointer">
-                        <span>
-                          <RxAvatar />
-                        </span>
-                        Employee Mode
-                      </li>
-                    </div>
-                  )}
-
-                  <li className="px-2 py-2 hover:bg-gray-200 flex items-center gap-2 text-teal-900 cursor-pointer">
-                    <VscSettings />
-                    Settings
-                  </li>
-                  <li
-                    className="px-2 py-2 hover:bg-gray-200  flex items-center gap-2 text-amber-700 cursor-pointer"
-                    onClick={() => handleLogout()}>
-                    <BiLogOut />
-                    Logout
-                  </li>
-                </ul>
-              </div>
-            )}
+            {dropdownIsOpen && <UserDropdown />}
           </div>
         </div>
       </div>
