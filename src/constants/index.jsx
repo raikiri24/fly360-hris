2b1e79ed5d9696e5eac94a0832bf1ad4dabd86b8
Paul Leandro Lanot <31089473+raikiri24@users.noreply.github.com>
2023-07-22 22:21:06 +0800

    changes in constants

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -1,7 +1,7 @@
 export const API_AUTH = '/authenticate';
 export const API_SEND_OTP = '/users/sendOtp';
-export const API_VERIFY_OTP = '/users/check-otp';
-export const API_RESET_PASSWORD = '/users/reset-password';
+export const API_VERIFY_OTP = '/users/checkOtp';
+export const API_RESET_PASSWORD = '/users/resetPassword';
 export const TOKEN_KEY = `CdbiDeqLhE_${import.meta.env.VITE_STAGE}`;
 export const USER_KEY = `sXbiDfqLhE_${import.meta.env.VITE_STAGE}`;
 export const SERVER_ERROR_MSG = '500 INTERNAL SERVER ERROR';
