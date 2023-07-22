2730a59b1e91e6ab1615ad8e51e6cac38475dbd6
Paul Leandro Lanot <31089473+raikiri24@users.noreply.github.com>
2023-07-22 21:29:26 +0800

    test

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -2,3 +2,4 @@ export const API_AUTH = '/authenticate';
 export const TOKEN_KEY = `CdbiDeqLhE_${import.meta.env.VITE_STAGE}`;
 export const USER_KEY = `sXbiDfqLhE_${import.meta.env.VITE_STAGE}`;
 export const SERVER_ERROR_MSG = '500 INTERNAL SERVER ERROR';
+export const TEST = '';
